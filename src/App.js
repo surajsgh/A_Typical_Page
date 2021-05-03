50bc34026885510910a489a366c109e58d0d9891
Suraj Singh <surajsingh220698@outlook.com>
2021-05-03 20:08:55 +0530

    Add prettierrc and add useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //  In useEffect, the first arguement is the anonymous function and the second arg is the array of dependencies.
+  useEffect(() => {
+    //  We've to write this part in useEffect hook, otherwise the app would fall into infinite loop.
+    //  Now, that we have written this part in useEffect hook, then this hook would run during compilation and during component re-evaluation.(Only dependency would change.)
+    const info = localStorage.getItem("isLoggedIn");
+    if (info === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    // Store in the local storage once the user is logged in.
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    //  storage gets cleared once the user is logged out.
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
